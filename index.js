5975694963d855f40e292c7cdb60581898f233fd
kfconeone <kfconeone@gmail.com>
2017-11-20 14:57:26 +0800

    大致完成，嘗試佈署到雲端

--- index.js
+++ index.js
@@ -1,16 +1,62 @@
-'use struct'
-const http = require("./http.js");
-const express = require('express');
-var app = express();
+var request = require('request');
+const { serveHttp, app } = require('webfunc')
 
-var bodyParser = require('body-parser');
+exports.adapter = serveHttp([
+    app.post('/{router}', (req, res,params) => {
+        adapter(req,res,params);
+    })
+    ])
 
-
-app.use(bodyParser.json());
-app.use("/adapter",function(req,res)
+function adapter(req,res,params)
 {
-    http.adapter(req,res);
-});
 
-var port = process.env.port || 8080
-app.listen(port)
+    let adapterOption = {
+        uri: 'http://localhost:52673/' + params.router,
+        method: 'POST',
+        json: params
+    };
+
+    request(adapterOption, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            res.status(200).send(body);
+            res.end();
+        }
+        else
+        {
+            res.status(500).send(response.statusCode);
+            res.end();
+        }
+    });
+
+}
+
+// exports.adapter = function (req, res) {
+//     let result = 
+//     {
+//         url:req.url,
+//         rawBody:req.body
+//     }
+//     res.send(result);
+//     res.end();
+//     let adapterOption = {
+//         uri: 'http://localhost:52673/' + req.body.uri,
+//         method: 'POST',
+//         json: req.body
+//     };
+
+//     request(adapterOption, function (error, response, body) {
+//         if (!error && response.statusCode == 200) {
+//             console.log(body) // Print the shortened url.
+//             res.status(200).send(body);
+//             res.end();
+//         }
+//         else
+//         {
+//             console.log(response.statusCode);
+//             res.status(500).send(response.statusCode);
+//             res.end();
+//         }
+//     });
+// };
+
+
