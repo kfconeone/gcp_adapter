971a3937e28555aaf98d089fda42086191b68a34
kfconeone <kfconeone@gmail.com>
2017-11-20 11:22:09 +0800

    gcp cloud function的Adapter

--- index.js
+++ index.js
@@ -1,35 +1,16 @@
-http = require('http');
+'use struct'
+const http = require("./http.js");
+const express = require('express');
+var app = express();
 
-var options = {
-    hostname: 'http://localhost:52673',
-      port: 80,
-      path: '/deviceLoginV2',
-      method: 'POST',
-      headers: {
-              'Content-Type': 'application/json',
-          }
-        };
-var req = http.request(options, function(res) {
-  console.log('Status: ' + res.statusCode);
-  console.log('Headers: ' + JSON.stringify(res.headers));
-  res.setEncoding('utf8');
-  res.on('data', function (body) {
-    console.log('Body: ' + body);
-    fs.writeFile("test.txt", body, function(err) {
-    if(err) {
-        return console.log(err);
-    }
-              console.log("The file was saved!");
-    }); 
-  });
+var bodyParser = require('body-parser');
+
+
+app.use(bodyParser.json());
+app.use("/adapter",function(req,res)
+{
+    http.adapter(req,res);
 });
-req.on('error', function(e) {
-  console.log('problem with request: ' + e.message);
-});
-// write data to request body
-// req.write('{"string": result}');  ///RESULT HERE IS A JSON
 
-result = '{ "hello": "json" }';
-req.write('{"string": '+result+'}');
-
-req.end();
+var port = process.env.port || 8080
+app.listen(port)
